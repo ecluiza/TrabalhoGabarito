60989e81ee6fccad0ee6978e6e59f2e87972066e
ecluiza <maecastr2@gmail.com>
2026-09-14 17:26:58 +0000

    remove exercicios de aula, repo agora e so do trabalho

--- Aula2Ex1.m
+++
@@ -1,9 +0,0 @@
-clear all;
-clc;
-
-imgIF = imread("if.jpg");
-
-imgIFG = rgb2gray(imgIF);
-
-figure(1),imshow(imgIF), title('Imagem Colorida');
-figure(2),imshow(imgIFG), title('Imagem Cinza');
--- Aula2Ex5.m
+++
@@ -1,8 +0,0 @@
-clear all;
-clc;
-
-a = imread('Lena512.bmp');
-b = imcrop(a,[100,100 180,180]);
-
-figure(1), subplot(2,1,1), colormap("gray"), imshow(a);
-figure(1), subplot(2,1,2), colormap("gray"), imshow(b);
